414421802d138b83f0745405c778492b849f9393
Kae Bartlett <kae@voxelfox.co.uk>
2023-05-29 10:55:58 +0100

    Attempt to add timezone handling to timestamp command

--- plugins/timestamp.py
+++ plugins/timestamp.py
@@ -1,116 +1,186 @@
-from datetime import datetime as dt, MINYEAR, MAXYEAR
+from datetime import datetime as dt, timedelta, timezone
 import re
 
 import novus
 from novus.ext import client
+import pytz
 
 
 class Timestamp(client.Plugin):
+
+    @staticmethod
+    def get_timezone_from_string(timezone_name: str) -> timezone | None:
+        """
+        Try and work out a timezone from an input string, in given priority
+        order:
+
+        * See if the string is a TZ name
+        * See if the string is in form UTC+XX:YY
+        * See if the string is one of our common locations
+            * US states
+            * European countries
+        * See if the string is one of our common acronyms
+        * Fallback to null
+        """
+
+        # TZ names
+        if timezone_name in pytz.common_timezones:
+            if timezone_name.casefold() == "GMT":
+                pass
+            else:
+                return pytz.timezone(timezone_name)
+
+        # UTC offsets
+        match = re.search(
+            r"^utc(?P<direction>[+-])(?P<hour>\d{1,2}(?::(?P<minute>\d{2}))?)$",
+            timezone_name,
+            re.IGNORECASE,
+        )
+        if match is not None:
+            delta = timedelta(
+                hours=int(match.group("hour")),
+                minutes=int(match.group("minute") or 0)
+            )
+            if match.group("direction") == "-":
+                delta = -delta
+            return timezone(delta)
+
+        # Common countries
+        # TODO
+
+        # Common acronyms
+        acronyms = {
+            "UTC": pytz.utc,
+
+            # Australia
+            "ACST": pytz.timezone("Australia/Adelaide"),
+            "ACST": pytz.timezone("Australia/Adelaide"),
+            "ACT": pytz.timezone("Australia/Adelaide"),
+            "AEST": pytz.timezone("Australia/Melbourne"),
+            "AEDT": pytz.timezone("Australia/Melbourne"),
+            "AET": pytz.timezone("Australia/Melbourne"),
+            "AWST": pytz.timezone("Australia/Perth"),
+            "AWDT": pytz.timezone("Australia/Perth"),
+
+            # America
+            "ET": pytz.timezone("America/New_York"),
+            "EST": pytz.timezone("America/New_York"),
+            "EDT": pytz.timezone("America/New_York"),
+            "CT": pytz.timezone("America/Chicago"),
+            "CST": pytz.timezone("America/Chicago"),
+            "CDT": pytz.timezone("America/Chicago"),
+            "MT": pytz.timezone("America/Phoenix"),
+            "MST": pytz.timezone("America/Phoenix"),
+            "MDT": pytz.timezone("America/Phoenix"),
+            "PT": pytz.timezone("America/Los_Angeles"),
+            "PST": pytz.timezone("America/Los_Angeles"),
+            "PDT": pytz.timezone("America/Los_Angeles"),
+
+            # Europe
+            "GMT": pytz.timezone("Europe/London"),
+            "BST": pytz.timezone("Europe/London"),
+            "CET": pytz.timezone("Brussels"),
+            "CEST": pytz.timezone("Brussels"),
+
+            # Africa
+            # Asia
+        }
+        if timezone_name.upper() in acronyms:
+            return acronyms[timezone_name.upper()]
+
+        # Fallback
+        return None
 
     @client.command(
         name="timestamp",
         options=[
             novus.ApplicationCommandOption(
-                name="time",
+                name="year",
+                type=novus.ApplicationOptionType.integer,
+                description="The year of the given datetime.",
+                min_value=2_000,
+                max_value=3_000,
+            ),
+            novus.ApplicationCommandOption(
+                name="month",
+                type=novus.ApplicationOptionType.integer,
+                description="The month of the given datetime.",
+                choices=[
+                    novus.ApplicationCommandChoice("January", 1),
+                    novus.ApplicationCommandChoice("February", 2),
+                    novus.ApplicationCommandChoice("March", 3),
+                    novus.ApplicationCommandChoice("April", 4),
+                    novus.ApplicationCommandChoice("May", 5),
+                    novus.ApplicationCommandChoice("June", 6),
+                    novus.ApplicationCommandChoice("July", 7),
+                    novus.ApplicationCommandChoice("August", 8),
+                    novus.ApplicationCommandChoice("September", 9),
+                    novus.ApplicationCommandChoice("October", 10),
+                    novus.ApplicationCommandChoice("November", 11),
+                    novus.ApplicationCommandChoice("December", 12),
+                ],
+            ),
+            novus.ApplicationCommandOption(
+                name="day",
+                type=novus.ApplicationOptionType.integer,
+                description="The day of the given datetime.",
+                min_value=1,
+                max_value=31,
+            ),
+            novus.ApplicationCommandOption(
+                name="hour",
+                type=novus.ApplicationOptionType.integer,
+                description="The hour of the given datetime, in 24 hour (military) time.",
+                min_value=0,
+                max_value=23,
+            ),
+            novus.ApplicationCommandOption(
+                name="minute",
+                type=novus.ApplicationOptionType.integer,
+                description="The minute of the given datetime.",
+                min_value=0,
+                max_value=59,
+            ),
+            novus.ApplicationCommandOption(
+                name="timezone",
                 type=novus.ApplicationOptionType.string,
-                description="The time to convert ((yy)yy/mm/dd) (hh:mm)",
-                required=True,
-            ),  
-            # novus.ApplicationCommandOption(
-            #     name="timezone",
-            #     type=novus.ApplicationOptionType.string,
-            #     description="The timezone of the provided timestamp",
-            #     required=False,
-            # ),
+                description="The timezone of the provided timestamp",
+            ),
         ],
     )
     async def timestamp(
             self,
-            interaction: novus.types.CommandI,
-            time: str) -> None:
+            ctx: novus.types.CommandI,
+            *,
+            year: int | None = None,
+            month: int | None = None,
+            day: int | None = None,
+            hour: int | None = None,
+            minute: int | None = None,
+            timezone: str = "UTC") -> None:
         """
         Sends a well-formatted Discord timestamp message.
         """
 
-        """
-        The time string is made up of two parts: y/m/d and h:m
-
-        Year can either be 2 digits (20yy) or 4 ditits (yyyy)
-        Month and Day can be up to 2 digts (mm dd)
-        Hour and Minute are both 1 or 2 digits (hh:mm)
-
-        If an attribute is invalid (month 13, for example), the current value 
-        for that attribute is used.
-        """
-        await interaction.defer()
-
-        # The default values for each datetime attribute is the current time
-        curr_time = dt.utcnow()
-        created_payload = {
-            'year': curr_time.year,
-            'month': curr_time.month,
-            'day': curr_time.day,
-
-            'hour': curr_time.hour,
-            'minute': curr_time.minute
-        }
-
-        # TODO: This could be moved into a util if needed
-        # Patterns for recognizing year-month-date and hour-minute
-        ymd_pattern = r"^(?:(?P<year>(?:\d{2})?\d{2})/)?(?P<month>\d{1,2})/(?P<day>\d{1,2})"
-        hm_pattern = r"(?P<hour>\d{1,2}):(?P<minute>\d{1,2})"
-
-        # Search the time string with both patterns
-        ymd_match = re.search(f"{ymd_pattern}", time)
-        hm_match = re.search(f"{hm_pattern}", time)
-
-        # If a year-month-date match is found
-        if ymd_match:
-            year, month, day = map(int, ymd_match.groups())
-
-            # Constrain and update the year
-            if year and MINYEAR <= year <= MAXYEAR:
-                
-                # If a two-digit year is provided
-                if year < 100:
-                    year += 2000 # Will have to update this to 3000 eventually
-
-                created_payload['year'] = year
-            
-            # Constrain and update the month
-            if month and 1 <= month <= 12:
-                created_payload['month'] = month
-            
-            # Constrain and update the day
-            # If there's an easy way to calculate how many days are in a month,
-            # feel free to change `31`
-            if day and 1 <= day <= 31:
-                created_payload['day'] = day
-            
-        # If a hour-minute match is found
-        if hm_match:
-            hour, minute = map(int, hm_match.groups())
-
-            # Constrain and update the hour
-            if hour and 0 <= hour < 24:
-                created_payload['hour'] = hour
-            
-            # Constrain and update the minute
-            if minute and 0 <= minute < 60:
-                created_payload['minute'] = minute
-        
-        # TODO: No idea how I would do timezones lol
-
-        try:
-            created = dt(**created_payload)
-            await interaction.send(f"<t:{int(created.timestamp())}>")
-        except:
-            # Send a confirmation message
-            await interaction.send(
-                "Something went wrong creating that timestamp :c"
+        # Get timezone so we can say if the timezone is valid
+        if (tz_object := self.get_timezone_from_string(timezone)) is None:
+            return await ctx.send(
+                f"The timezone `{timezone}` is not valid.",
+                ephemeral=True,
             )
 
-
-
-
-
+        # The default values for each datetime attribute is the current time
+        created_time = dt.utcnow()
+        default = lambda a, b: a if a is not None else b
+        created_time = created_time.replace(
+            year=default(year, created_time.year),
+            month=default(month, created_time.month),
+            day=default(day, created_time.day),
+            hour=default(hour, created_time.hour),
+            minute=default(minute, created_time.minute),
+            second=0,
+            microsecond=0,
+            tzinfo=tz_object,
+        )
+        timestamp: str = format(created_time.timestamp(), ".0f")
+        await ctx.send(f"<t:{timestamp}> (`<t:{timestamp}>`)")
