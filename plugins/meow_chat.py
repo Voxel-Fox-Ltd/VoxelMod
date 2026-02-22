42b351d23e5332a946559394e7d033fa7380773c
Kae Bartlett <kae@voxelfox.co.uk>
2026-02-22 21:36:36 +0000

    Add meow chat

---
+++ plugins/meow_chat.py
@@ -0,0 +1,168 @@
+"""
+Copyright (c) Kae Bartlett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from __future__ import annotations
+
+import asyncio
+import re
+
+from novus.ext import client
+import novus as n
+from novus import types as t
+
+import utils as u
+
+
+class MeowChat(client.Plugin):
+
+    MEOW_CHATS: set[int] = set()
+    MEOW_KEYWORDS: set[str | re.Pattern] = {
+        "meow",
+        "mrow",
+        "mew",
+        "nya",
+        "x3",
+        ":3",
+        "=3",
+        "uwu",
+        "owo",
+        "rawr",
+        "yip",
+        re.compile(r"\bmrr+p\b", re.IGNORECASE),
+    }
+    MEOW_TIMEOUT_TASKS: dict[int, asyncio.Task] = {}
+
+    @staticmethod
+    def match(pattern: str | re.Pattern, string: str) -> bool:
+        if isinstance(pattern, str):
+            return pattern in string.lower()
+        return bool(pattern.search(string))
+
+    @client.event.message
+    async def on_message(self, message: n.Message) -> None:
+        """
+        Force the user to speak like a cat if they have meow chat enabled.
+        """
+
+        if message.author.bot:
+            return  # ignore bots
+
+        if message.guild is None:
+            return  # ignore DMs
+
+        assert isinstance(message.author, n.GuildMember)
+        assert message.author.permissions is not None
+
+        if message.author.permissions.manage_messages:
+            return  # ignore mods
+
+        if message.channel.id not in self.MEOW_CHATS:
+            return  # ignore channels that don't have meow chat enabled
+
+        if not any(self.match(pattern, message.content) for pattern in self.MEOW_KEYWORDS):
+            try:
+                await message.delete(reason="Meow chat enabled; invalid message.")
+            except (n.Forbidden, n.NotFound):
+                pass
+            except Exception as e:
+                self.log.exception(
+                    "Failed to delete message in meow chat channel %d: %s",
+                    message.channel.id, e,
+                )
+
+    @client.command(
+        "meow-chat enable",
+        options=[
+            n.ApplicationCommandOption(
+                name="time",
+                description="Duration to enable meow chat for (e.g. '10m' for 10 minutes).",
+                type=n.ApplicationOptionType.STRING,
+                required=False,
+            ),
+        ],
+        dm_permission=False,
+    )
+    async def enable_meowchat(self, ctx: t.CommandGI, time: str | None = None) -> None:
+        """
+        Enable meow chat in the current channel.
+        """
+
+        delta = None
+        if time:
+            try:
+                delta = u.get_datetime_until(time, default_days=None)
+            except ValueError:
+                await ctx.send(
+                    (
+                        "Invalid time provided; please provide a valid time "
+                        "(e.g. '10m' for 10 minutes)."
+                    ),
+                    ephemeral=True,
+                )
+                return
+        self.MEOW_CHATS.add(ctx.channel.id)
+
+        if delta:
+            future = n.utils.utcnow() + delta
+            await ctx.send(
+                f"Meow chat has been enabled! It will be automatically "
+                f"disabled {future.format('R')} nya :3"
+            )
+            if ctx.channel.id in self.MEOW_TIMEOUT_TASKS:
+                self.MEOW_TIMEOUT_TASKS[ctx.channel.id].cancel()
+            task = asyncio.create_task(
+                self.disable_meowchat_after_timeout(ctx.channel, future)
+            )
+            self.MEOW_TIMEOUT_TASKS[ctx.channel.id] = task
+        else:
+            await ctx.send("Meow chat has been enabled nya :3")
+
+    async def disable_meowchat_after_timeout(
+            self,
+            channel: n.Channel,
+            time: n.utils.DiscordDatetime) -> None:
+        """
+        Wait until the given timestamp and then disable meow chat in the given channel.
+        """
+
+        duration = time - n.utils.utcnow()
+        if duration.total_seconds() > 30:
+            await asyncio.sleep(duration.total_seconds() - 30)
+        while n.utils.utcnow() < time:
+            await asyncio.sleep(0.5)
+        self.MEOW_CHATS.discard(channel.id)
+        await channel.send("Meow chat has been automatically disabled :3")
+        self.MEOW_TIMEOUT_TASKS.pop(channel.id, None)
+
+    @client.command(
+        "meow-chat disable",
+        dm_permission=False,
+    )
+    async def disable_meowchat(self, ctx: t.CommandGI) -> None:
+        """
+        Disable meow chat in the current channel.
+        """
+
+        if ctx.channel.id not in self.MEOW_CHATS:
+            await ctx.send("Meow chat is not enabled in this channel.", ephemeral=True)
+            return
+
+        self.MEOW_CHATS.discard(ctx.channel.id)
+        if ctx.channel.id in self.MEOW_TIMEOUT_TASKS:
+            self.MEOW_TIMEOUT_TASKS[ctx.channel.id].cancel()
+            self.MEOW_TIMEOUT_TASKS.pop(ctx.channel.id, None)
+        await ctx.send("Meow chat has been disabled uwu :3")
